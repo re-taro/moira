b5f2e85ec02adeae4d3a12d5de4dce625c1707f2
Rintaro Itokawa <me@re-taro.dev>
2023-05-03 17:48:44 +0900

    feat: implement types for moira

---
+++ types/mod.ts
@@ -0,0 +1,24 @@
+export type Ok = { name: string; ok: true; message?: undefined };
+export type Err = {
+  name: string;
+  ok: false;
+  message: string;
+};
+export type Result = Ok | Err;
+
+export type RunResult =
+  | { check: Ok; run?: undefined }
+  | {
+    check: Err;
+    run: Result;
+  };
+
+export type Action = {
+  run: () => Promise<void>;
+  dry: () => Promise<Result>;
+};
+
+export type Task = {
+  check: () => Promise<ReadonlyArray<Result>>;
+  run: () => Promise<ReadonlyArray<RunResult>>;
+};
